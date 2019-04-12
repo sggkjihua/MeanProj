d12f0b051e122638f7162394bf7c057cb47b6a88
sggkjihua <sggkjihua@163.com>
2019-04-13 04:34:03 +0800

    material module

--- app/posts/post-create/post-create.component.ts
+++ app/posts/post-create/post-create.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
   newPost="";
   enteredValue = "";
-
+  onAddPost(){
+    this.newPost = this.enteredValue;
+  }
   /*
   onAddPost(postInput:HTMLTextAreaElement){
     this.newPost = postInput.value;
