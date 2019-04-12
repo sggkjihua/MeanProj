ef14dcd1a758a44801b5de58583f46944be60d51
sggkjihua <sggkjihua@163.com>
2019-04-13 03:39:12 +0800

    get user input

--- app/posts/post-create/post-create.component.ts
+++ app/posts/post-create/post-create.component.ts
@@ -5,5 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
+  newPost="";
+  enteredValue = "";
 
+  /*
+  onAddPost(postInput:HTMLTextAreaElement){
+    this.newPost = postInput.value;
+    console.log(postInput);
+  }
+  */
+    /* used to pop up the alert */
+    /*alert("Post added!");*/
 }
